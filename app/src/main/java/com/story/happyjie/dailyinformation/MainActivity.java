a20eeb9da09339093c1a114e2769b566a24a731e
llj <happyjie.1988@163.com>
2017-12-08 17:30:12 +0800

    添加BaseActivity

--- app/src/main/java/com/story/happyjie/dailyinformation/MainActivity.java
+++ app/src/main/java/com/story/happyjie/dailyinformation/MainActivity.java
@@ -1,12 +1,22 @@
 package com.story.happyjie.dailyinformation;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.story.happyjie.dailyinformation.base.BaseActivity;
+import com.story.happyjie.dailyinformation.databinding.ActivityMainBinding;
 
 /**
  * Created by llj on 2017/12/7.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showContentView();
+        mViewBinding.tvTest.setText("隔壁老王");
+    }
 }
